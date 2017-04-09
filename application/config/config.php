ee3052337ec98243fa653c7daae4b196e82859fd
rance <rance@hiwd.biz>
2017-04-09 14:57:05 -0400

    Switching to new hosting for hiwdtestlab

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://volunteerwizard.com/testlab';
+
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -370,7 +371,7 @@ $config['encryption_key'] = 'hiwdIsWhatshot';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = 'C:\Program Files (x86)\Ampps\www\99.62.19.56\testlab\application\test_sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
