edcac3c67decd59cd42946928b9fb879998cd0a1
rance <rance@hiwd.biz>
2017-04-11 19:26:50 -0400

    Working on signup form and validation.

--- application/controllers/Signup.php
+++ application/controllers/Signup.php
@@ -13,21 +13,18 @@ class Signup extends CI_Controller
 	function index()
 	{
 		// set form validation rules
-		$this->form_validation->set_rules('fname', 'First Name', 'trim|required|alpha|min_length[3]|max_length[30]|xss_clean');
+		/*$this->form_validation->set_rules('fname', 'First Name', 'trim|required|alpha|min_length[3]|max_length[30]|xss_clean');
 		$this->form_validation->set_rules('lname', 'Last Name', 'trim|required|alpha|min_length[3]|max_length[30]|xss_clean');
 		$this->form_validation->set_rules('email', 'Email ID', 'trim|required|valid_email|is_unique[user.email]');
 		$this->form_validation->set_rules('password', 'Password', 'trim|required|matches[cpassword]|md5');
-		$this->form_validation->set_rules('cpassword', 'Confirm Password', 'trim|required');
+		$this->form_validation->set_rules('cpassword', 'Confirm Password', 'trim|required');*/
 		
 		// submit
-		if ($this->form_validation->run() == FALSE)
-        {
-			// fails
+		/*if ($this->form_validation->run() == FALSE){
+			
 			$this->load->view('signup_view');
-        }
-		else
-		{
-			//insert user details into db
+                }else{
+			
 			$data = array(
 				'fname' => $this->input->post('fname'),
 				'lname' => $this->input->post('lname'),
@@ -35,17 +32,31 @@ class Signup extends CI_Controller
 				'password' => $this->input->post('password')
 			);
 			
-			if ($this->user_model->insert_user($data))
-			{
+			if ($this->user_model->insert_user($data)){
 				$this->session->set_flashdata('msg','<div class="alert alert-success text-center">You are Successfully Registered! Please login to access your Profile!</div>');
 				redirect('signup/index');
+			}else{
+				
+				$this->session->set_flashdata('msg','<div class="alert alert-danger text-center">Oops! Error.  Please try again later!!!</div>');
+				redirect('signup/index');
 			}
-			else
-			{
+		}*/
+                
+                //insert user details into db
+			$data = array(
+				'fname' => $this->input->post('fname'),
+				'lname' => $this->input->post('lname'),
+				'email' => $this->input->post('email'),
+				'password' => $this->input->post('password')
+			);
+			
+			if ($this->user_model->insert_user($data)){
+				$this->session->set_flashdata('msg','<div class="alert alert-success text-center">You are Successfully Registered! Please login to access your Profile!</div>');
+				redirect('signup/index');
+			}else{
 				// error
 				$this->session->set_flashdata('msg','<div class="alert alert-danger text-center">Oops! Error.  Please try again later!!!</div>');
 				redirect('signup/index');
 			}
-		}
 	}
 }
