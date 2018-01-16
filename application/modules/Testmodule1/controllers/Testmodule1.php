3f487451e58af85779692986ec6713b80c0f9864
ranceforhiwd <ranceaaron941@gmail.com>
2018-01-15 21:38:08 -0500

    specify package as a test

--- application/modules/Testmodule1/controllers/Testmodule1.php
+++ application/modules/Testmodule1/controllers/Testmodule1.php
@@ -1,36 +1,26 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Testmodule1 extends MX_Controller {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
-        
+/**
+ * This is the main module controller
+ * @author Rance Aaron
+ * @package Testmodule1
+ */
+class Testmodule1 extends MX_Controller {        
         public function __construct(){
 		parent::__construct();
 		$this->load->helper(array('url', 'html'));
 		$this->load->library('session');
-                $this->load->database();
+        $this->load->database();
 		$this->load->model('testmodule1_model');
 	}
     
 	public function index(){                
 		$this->load->view('testmodule1_view');
 	}
-        
+        /**
+		 * @function getData 
+		 */
         public function getData(){
             $output = array(
                 'draw'=>1,
